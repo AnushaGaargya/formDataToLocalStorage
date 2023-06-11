dbc0d46aa9f0bc051b5f6bea7681ff085d338c08
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-11 17:16:14 +0530

    storing user details in an object

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ form.addEventListener('submit', addToLocal);
 
 function addToLocal(e){
     e.preventDefault();
+    var myObj = new Object();
     var email = document.getElementById('email').value;
     var pword = document.getElementById('pw').value;
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", pword);
+    myObj["email"] = email
+    myObj["password"] = pword
+    let myObj_serialized = JSON.stringify(myObj);
+    localStorage.setItem("myObj", myObj_serialized);
+
+
+    // localStorage.setItem("email", email);
+    // localStorage.setItem("password", pword);
 
 }
 
 
-console.log("Hi");
+// console.log("Hi");
 // localStorage.setItem("email",x );
