6c560c398265583b7eb1bca0aefefa9d38011a4c
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-12 00:23:11 +0530

    added edit functionality

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ function addToLocal(e){
     deleteBtn.appendChild(document.createTextNode('Delete'));
     paragraph.appendChild(deleteBtn);
     deleteBtn.addEventListener("click", delete_fn);
+        // create edit button
+    var editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-warning btn-sm float-right edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    paragraph.appendChild(editBtn);
+    editBtn.addEventListener("click", edit_fn);
+
 
     function delete_fn(e){
         if(e.target.classList.contains('delete')){
@@ -37,10 +44,27 @@ function addToLocal(e){
                 localStorage.removeItem(first);
                 console.log(first)
             }
-    
-    
         }
-    
+    }
+
+    function edit_fn(e){
+        if(e.target.classList.contains('edit')){
+            
+        var item = e.target.parentElement; //the parent element of edit btn is list item. 
+        document.getElementById("test").removeChild(item);
+        const first = (item.textContent).split(' ')[0]
+        x = localStorage.getItem(first)
+        const usr_obj = JSON.parse(x);
+        console.log(usr_obj["email"]);
+        
+        var em = document.getElementById("email");
+        em.value = usr_obj["email"]
+        var ph = document.getElementById("pw")
+        ph.value = usr_obj["phone"]
+
+        localStorage.removeItem(first);
+            
+        }
     }
     
 
